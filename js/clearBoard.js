d482a997092c3f5335a93f3dbe9c680d1a46e1d6
Łukasz <lukasz.rdzanek@protonmail.com>
2019-04-28 20:46:33 +0200

    Clear board button with script

---
+++ js/clearBoard.js
@@ -0,0 +1,12 @@
+
+const button = document.getElementById('clear');
+button.onclick = function () {
+    const board = document.querySelectorAll(".task__description");
+    Array.from(board).map((el, i) => {
+        el.remove();
+    });
+};
+
+
+
+
